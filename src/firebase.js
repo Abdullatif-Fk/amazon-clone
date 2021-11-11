314c383acbf2ff643e8ecf1496db14cc83d4a73c
Abdullatif-Fk <abdul647.fk@gmail.com>
2021-11-12 00:45:40 +0200

    adding home page

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAeNSbnxxENGP7DefB6otjO4K2u0VLqQSg",
+  authDomain: "e-clone-f6650.firebaseapp.com",
+  projectId: "e-clone-f6650",
+  storageBucket: "e-clone-f6650.appspot.com",
+  messagingSenderId: "313313253018",
+  appId: "1:313313253018:web:6f15cddb8d455a47747123",
+  measurementId: "G-4FTVR6RXG2",
+};
